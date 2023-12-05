a67a393ab0663b5e5ac94eb78489ce66a6c5893d
Petar Mitkov <ph@Petars-MacBook-Pro.local>
2023-12-05 19:56:10 +0200

    Adding gcp_snapshots_add_labels_between_dates.sh script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gcp-scripts
 Scripts for managing GCP resources
+
+## gcp_snapshots_add_labels_between_dates.sh
+
+Script to add labels to Compute Disk Snapshots created between 2 dates
+
+Usage: 
+```sh
+gcp_snapshots_add_labels_between_dates.sh [start-date-utc] [end-date-utc] [label-key=label-value]
+```
+Example: 
+```sh 
+gcp_snapshots_add_labels_between_dates.sh [2023-01-01] [2023-02-01] [component=disk-snapshot]
+```
---
+++ gcp_snapshots_add_labels_between_dates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; 
+then
+    >&2 echo "No arguments provided"
+    >&2 echo "Usage: gcp_snapshots_add_labels_between_dates.sh [start-date-utc] [end-date-utc] [label-key=label-value]"
+    >&2 echo "Example: gcp_snapshots_add_labels_between_dates.sh [2023-01-01] [2023-02-01] [component=disk-snapshot]"
+    exit 1
+fi
+
+start_date=$1
+end_date=$2
+
+snapshots=`gcloud compute snapshots list --format="value(name)" --filter="creationTimestamp>='${start_date}' AND creationTimestamp<='${end_date}'"`
+
+for snapshot in $snapshots; 
+do 
+    gcloud compute snapshots add-labels "${snapshot}" --labels="$3"
